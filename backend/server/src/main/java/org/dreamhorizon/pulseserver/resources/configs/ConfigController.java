3e384e295f1ee92c4c7c2fbd0469dc769db22910
abhishek-dream11 <abhishek.kumar@dream11.com>
2025-12-04 17:44:01 +0530

    feat: added basic structure of config module

--- backend/server/src/main/java/org/dreamhorizon/pulseserver/resources/configs/ConfigController.java
+++ backend/server/src/main/java/org/dreamhorizon/pulseserver/resources/configs/ConfigController.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequiredArgsConstructor(onConstructor = @__({@Inject}))
-@Path("/v1/pulse-configs")
+@Path("/v1/configs")
 public class ConfigController {
 
 }
