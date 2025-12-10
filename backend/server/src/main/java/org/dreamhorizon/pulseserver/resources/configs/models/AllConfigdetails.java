2f5368f579b851a898e3bd9a23b065d0ca3fd558
hemanthbee <hemanth.naidu@gmail.com>
2025-12-10 09:56:48 +0530

    added dto files

---
+++ backend/server/src/main/java/org/dreamhorizon/pulseserver/resources/configs/models/AllConfigdetails.java
@@ -0,0 +1,28 @@
+package org.dreamhorizon.pulseserver.resources.configs.models;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AllConfigdetails {
+    private List<Configdetails> configDetails;
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Configdetails {
+        private long version;
+        private boolean isactive;
+        private String description;
+        private String createdBy;
+        private String createdAt;
+    }
+}
