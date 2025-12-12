13de3239bc9a162514fc9769ea3596873137551f
Hemant Gehlot <hemant.gehlot@dream11.com>
2025-12-12 20:11:38 +0530

    UnnecessaryAbstractClass -fixed

--- pulse-android-otel/pulse-upload-plugin/src/main/kotlin/com/pulse/extensions/PulseSourceMapsExtension.kt
+++ pulse-android-otel/pulse-upload-plugin/src/main/kotlin/com/pulse/extensions/PulseSourceMapsExtension.kt
@@ -1,16 +1,19 @@
 package com.pulse.extensions
 
 import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
-import javax.inject.Inject
 
-abstract class PulseSourceMapsExtension @Inject constructor(
-    @Suppress("UnusedPrivateProperty") objects: ObjectFactory
-) {
+abstract class PulseSourceMapsExtension {
     abstract val apiUrl: Property<String>
     abstract val mappingFile: RegularFileProperty
     abstract val appVersion: Property<String>
     abstract val versionCode: Property<Int>
+
+    fun isConfigured(): Boolean {
+        return apiUrl.isPresent &&
+            mappingFile.isPresent &&
+            appVersion.isPresent &&
+            versionCode.isPresent
+    }
 }
 
