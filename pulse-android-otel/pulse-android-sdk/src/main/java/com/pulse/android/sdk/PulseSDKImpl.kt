2f01ac1683292e19dfbcc188d0d96b3502cd7425
Atul Gupta <atul.gupta@dream11.com>
2025-12-26 19:26:30 +0530

    Apply spotless and detekt

--- pulse-android-otel/pulse-android-sdk/src/main/java/com/pulse/android/sdk/PulseSDKImpl.kt
+++ pulse-android-otel/pulse-android-sdk/src/main/java/com/pulse/android/sdk/PulseSDKImpl.kt
@@ -150,10 +150,11 @@ internal class PulseSDKImpl :
 
         val attrRejects = mutableMapOf<AttributeKey<*>, Predicate<*>>()
         attrRejects[AttributeKey.booleanKey("pulse.internal")] = Predicate<Boolean> { it == true }
-        val filteredSpanExporter = FilteringSpanExporter
-            .builder(otlpSpanExporter)
-            .rejectSpansWithAttributesMatching(attrRejects)
-            .build()
+        val filteredSpanExporter =
+            FilteringSpanExporter
+                .builder(otlpSpanExporter)
+                .rejectSpansWithAttributesMatching(attrRejects)
+                .build()
 
         val otlpLogExporter: LogRecordExporter =
             OtlpHttpLogRecordExporter
