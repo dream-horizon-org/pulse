a152b70a8b71e433a4d2122c1cc3f6da7dab6506
Atul Gupta <atul.gupta@dream11.com>
2025-12-26 11:41:58 +0530

    Add frame data based on timestamp of the spans

--- pulse-android-otel/instrumentation/slowrendering/src/main/java/io/opentelemetry/android/instrumentation/slowrendering/SpanFrameAttributesAppended.kt
+++ pulse-android-otel/instrumentation/slowrendering/src/main/java/io/opentelemetry/android/instrumentation/slowrendering/SpanFrameAttributesAppended.kt
@@ -1,66 +1,59 @@
 package io.opentelemetry.android.instrumentation.slowrendering
 
 import io.opentelemetry.api.common.Attributes
-import io.opentelemetry.api.trace.Span
 import io.opentelemetry.context.Context
 import io.opentelemetry.sdk.trace.ReadWriteSpan
 import io.opentelemetry.sdk.trace.ReadableSpan
 import io.opentelemetry.sdk.trace.internal.ExtendedSpanProcessor
-import java.util.concurrent.ConcurrentHashMap
 
 class SpanFrameAttributesAppended : ExtendedSpanProcessor {
-    private val spanMap =
-        ConcurrentHashMap<Span, SlowRenderListener.Companion.CumulativeFrameData>()
-
     override fun onStart(
         parentContext: Context,
         span: ReadWriteSpan,
     ) {
-        spanMap[span] = SlowRenderListener.createCumulativeFrameMetric()
+        // no-op
     }
 
-    override fun isStartRequired() = true
+    override fun isStartRequired(): Boolean = false
 
     override fun onEnd(span: ReadableSpan) {}
 
     override fun isEndRequired(): Boolean = false
 
     override fun onEnding(span: ReadWriteSpan) {
-        spanMap[span]?.let {
-            span.setAllAttributes(
-                getFrameAttributes(
-                    it,
-                    SlowRenderListener.createCumulativeFrameMetric(),
+        val spanData = span.toSpanData()
+        span.setAllAttributes(
+            getFrameAttributes(
+                FrameDataHelper.createCumulativeFrameMetric(
+                    spanData.startEpochNanos / 1000,
+                    spanData.endEpochNanos / 1000,
                 ),
-            )
-        }
+            ),
+        )
     }
 
     override fun isOnEndingRequired(): Boolean = true
 
     internal companion object {
-        private fun getFrameAttributes(
-            first: SlowRenderListener.Companion.CumulativeFrameData,
-            second: SlowRenderListener.Companion.CumulativeFrameData,
-        ): Attributes =
+        private fun getFrameAttributes(first: FrameDataHelper.CumulativeFrameData): Attributes =
             Attributes
                 .builder()
                 .apply {
                     put(
                         "app.interaction.analysed_frame_count",
-                        second.analysedFrameCount - first.analysedFrameCount,
+                        first.analysedFrameCount,
                     )
                     put(
                         "app.interaction.unanalysed_frame_count",
-                        second.unanalysedFrameCount - first.unanalysedFrameCount,
+                        first.unanalysedFrameCount,
                     )
                     put(
                         "app.interaction.slow_frame_count",
-                        second.slowFrameCount - first.slowFrameCount,
+                        first.slowFrameCount,
                     )
                     put(
                         "app.interaction.frozen_frame_count",
-                        second.frozenFrameCount - first.frozenFrameCount,
+                        first.frozenFrameCount,
                     )
                 }.build()
     }
