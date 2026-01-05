62c4eed18c8d9a05a43748032028f11d790a60ae
Sarabjyot123 <sarabjyot.singh@dream11.com>
2026-01-05 15:19:00 +0530

    Fix Clickhouse

--- deploy/terraform/clickhouse/main.tf
+++ deploy/terraform/clickhouse/main.tf
@@ -13,6 +13,13 @@ provider "aws" {
   region = var.aws_region
 }
 
+############################################
+# ADD: Discover VPC ID from one of the subnets
+############################################
+data "aws_subnet" "any_clickhouse_subnet" {
+  id = values(var.subnets)[0]
+}
+
 locals {
   azs = sort(keys(var.subnets))  # stable ordering
 
@@ -31,6 +38,93 @@ locals {
       az            = local.azs[pair[1]]                 # replica 1 -> az[0], replica 2 -> az[1], etc.
       subnet_id     = var.subnets[local.azs[pair[1]]]
     }
+  }
+
+  # Subnets where ClickHouse nodes exist (unique list)
+  clickhouse_subnet_ids = distinct([
+    for _, v in local.clickhouse_instances : v.subnet_id
+  ])
+}
+
+############################################
+# ADD: NLB for ClickHouse Native Port 9000
+############################################
+resource "aws_lb" "clickhouse_nlb" {
+  name               = "${var.cluster_name}-nlb"
+  internal           = true
+  load_balancer_type = "network"
+
+  # Put NLB only in the AZs/subnets where ClickHouse nodes exist
+  subnets = local.clickhouse_subnet_ids
+
+  # For your case (clients in 3 AZs, CH in 2 AZs), keep this ON for smoother routing/failover
+  enable_cross_zone_load_balancing = true
+
+  enable_deletion_protection = false
+
+  security_groups = var.vpc_security_group_ids
+
+  tags = {
+    Name        = "${var.cluster_name}-nlb"
+    ClusterName = var.cluster_name
+    service     = "pulse"
+  }
+}
+
+resource "aws_lb_target_group" "clickhouse_tg" {
+  name        = "${var.cluster_name}-tg"
+  port        = 9000
+  protocol    = "TCP"
+  vpc_id      = data.aws_subnet.any_clickhouse_subnet.vpc_id
+  target_type = "instance"
+
+  health_check {
+    protocol            = "TCP"
+    port                = "9000"
+    interval            = 10
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name        = "${var.cluster_name}-tg"
+    ClusterName = var.cluster_name
+    service     = "pulse"
+  }
+}
+
+resource "aws_lb_listener" "clickhouse_9000" {
+  load_balancer_arn = aws_lb.clickhouse_nlb.arn
+  port              = 9000
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.clickhouse_tg.arn
+  }
+}
+
+############################################
+# ADD: Attach all ClickHouse EC2 instances (all shard/replica nodes) to the NLB TG
+############################################
+resource "aws_lb_target_group_attachment" "clickhouse_attach" {
+  for_each = aws_instance.clickhouse
+
+  target_group_arn = aws_lb_target_group.clickhouse_tg.arn
+  target_id        = each.value.id
+  port             = 9000
+}
+
+
+resource "aws_route53_record" "clickhouse_nlb" {
+  zone_id = var.private_zone_id
+  name    = "${var.cluster_name}.${var.private_zone_domain}"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.clickhouse_nlb.dns_name
+    zone_id                = aws_lb.clickhouse_nlb.zone_id
+    evaluate_target_health = true
   }
 }
 
@@ -188,4 +282,16 @@ output "clickhouse_hostnames" {
   value = [for k in keys(local.clickhouse_instances) :
     "${k}.${var.cluster_name}.${var.private_zone_domain}"
   ]
+}
+
+output "clickhouse_nlb_dns_name" {
+  value = aws_lb.clickhouse_nlb.dns_name
+}
+
+output "clickhouse_nlb_endpoint_9000" {
+  value = "${aws_lb.clickhouse_nlb.dns_name}:9000"
+}
+
+output "clickhouse_private_endpoint" {
+  value = "${var.cluster_name}.${var.private_zone_domain}:9000"
 }
