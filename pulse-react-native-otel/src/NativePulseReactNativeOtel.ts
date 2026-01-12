f9463b6950457d71a6a13efcbbd85fbe1d856292
shubhamguptadream11 <shubham.gupta@dream11.com>
2026-01-12 16:26:01 +0530

    fix: comment fix

--- pulse-react-native-otel/src/NativePulseReactNativeOtel.ts
+++ pulse-react-native-otel/src/NativePulseReactNativeOtel.ts
@@ -59,7 +59,7 @@ export interface Spec extends TurboModule {
   /** Set the current React Native screen name to sync active screen name on Android/iOS */
   setCurrentScreenName(screenName: string): boolean;
 
-  /** Get all features and their enabled/disabled status from cached config */
+  /** Get all SDK Remote Config features */
   getAllFeatures(): Object;
 }
 
