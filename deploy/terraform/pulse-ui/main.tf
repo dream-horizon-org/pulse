68fc2236917d8acb50b3990131d2a02b62b46276
Mohd. Yasir Ansari <yasir.ansari@dream11.com>
2026-02-26 01:43:56 +0530

    chore: corrected R53

--- deploy/terraform/pulse-ui/main.tf
+++ deploy/terraform/pulse-ui/main.tf
@@ -281,7 +281,7 @@ resource "aws_autoscaling_group" "pulse_ui" {
 # -------------------------------------------------------------------
 resource "aws_route53_record" "pulse_ui" {
   zone_id = var.route53_zone_id
-  name    = "fancode1"
+  name    = var.route53_record_name
   type    = "A"
 
   alias {
