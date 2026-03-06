701f574dc8c460b06181c68ce13db14e11f0a352
Mohd. Yasir Ansari <yasir.ansari@dream11.com>
2026-03-06 12:21:51 +0530

    chore: added log line

--- backend/server/src/main/java/org/dreamhorizon/pulseserver/verticle/MainVerticle.java
+++ backend/server/src/main/java/org/dreamhorizon/pulseserver/verticle/MainVerticle.java
@@ -49,6 +49,7 @@ public class MainVerticle extends AbstractVerticle {
           this.webClient = WebClient.create(vertx, getWebClientOptions(webClientConfig));
           SharedDataUtils.put(vertx.getDelegate(), appConfig.mapTo(ApplicationConfig.class));
           JsonObject chConfig = config.getJsonObject("clickhouse", new JsonObject());
+          log.info("Clickhouse Config :: {}", chConfig);
           SharedDataUtils.put(vertx.getDelegate(), chConfig.mapTo(ClickhouseConfig.class));
           JsonObject athenaConfig = config.getJsonObject("athena", new JsonObject());
           SharedDataUtils.put(vertx.getDelegate(), athenaConfig.mapTo(AthenaConfig.class));
