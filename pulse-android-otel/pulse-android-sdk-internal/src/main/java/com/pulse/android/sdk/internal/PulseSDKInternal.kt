9fc11ee9b8acf15587a59c6b9447565e065be853
anirudhbharti <anirudh.bharti@dream11.com>
2026-03-13 14:19:58 +0530

    fix: add feature name in congig api and default url handling

--- pulse-android-otel/pulse-android-sdk-internal/src/main/java/com/pulse/android/sdk/internal/PulseSDKInternal.kt
+++ pulse-android-otel/pulse-android-sdk-internal/src/main/java/com/pulse/android/sdk/internal/PulseSDKInternal.kt
@@ -449,8 +449,8 @@ public class PulseSDKInternal : CoroutineScope by MainScope() {
             flushIntervalSeconds = featureConfig.flushIntervalSeconds ?: base.flushIntervalSeconds,
             flushAt = featureConfig.flushAt ?: base.flushAt,
             maxBatchSize = featureConfig.maxBatchSize ?: base.maxBatchSize,
-            replayApiBaseUrl = (featureConfig.replayApiBaseUrl?.takeIf { it.isNotBlank() } ?: base.replayApiBaseUrl)
-                ?.replace("://localhost", "://10.0.2.2"),
+            replayApiBaseUrl = (featureConfig.replayApiBaseUrl?.takeIf { it.isNotBlank() } ?: base.replayApiBaseUrl ?: DEFAULT_REPLAY_API_URL)
+                .replace("://localhost", "://10.0.2.2"),
         )
     }
 
@@ -783,6 +783,7 @@ public class PulseSDKInternal : CoroutineScope by MainScope() {
         internal const val CUSTOM_NON_FATAL_EVENT_NAME = "pulse.custom_non_fatal"
         private const val TAG = "AndroidSDK"
         private const val API_KEY_HEADER = "X-API-KEY"
+        private const val DEFAULT_REPLAY_API_URL = "http://localhost:3400"
         private const val METERING_SESSION_HEADER_KEY = "X-Pulse-Metering-Session-ID"
 
         internal object PrefsName {
