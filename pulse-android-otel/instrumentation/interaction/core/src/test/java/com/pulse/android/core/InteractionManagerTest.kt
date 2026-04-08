b65cfc69b26cef7aea87f9f2535ccbad4f51f332
Hemant Gehlot <hemant.gehlot@dream11.com>
2026-04-08 11:42:08 +0530

    spoteless applied

--- pulse-android-otel/instrumentation/interaction/core/src/test/java/com/pulse/android/core/InteractionManagerTest.kt
+++ pulse-android-otel/instrumentation/interaction/core/src/test/java/com/pulse/android/core/InteractionManagerTest.kt
@@ -1395,8 +1395,8 @@ class InteractionManagerTest {
                                 InteractionRemoteFakeUtils.createFakeInteractionEvent(
                                     name = "event2",
                                 ),
-                            )
-                    )
+                            ),
+                    ),
                 )
                 addEventWithNanoTimeFromBoot("event1")
                 addEventWithNanoTimeFromBoot("event2")
