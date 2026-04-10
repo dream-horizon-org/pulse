e29205f067807de41c5c034623584cc82a9bd714
Atul Gupta <atul.gupta@dream11.com>
2026-04-10 17:29:01 +0530

    Remove ignore annotation
    Update `var`

--- backend/server/src/main/kotlin/org/dreamhorizon/pulseserver/service/configs/models/MetricsToAddTarget.kt
+++ backend/server/src/main/kotlin/org/dreamhorizon/pulseserver/service/configs/models/MetricsToAddTarget.kt
@@ -1,6 +1,5 @@
 package org.dreamhorizon.pulseserver.service.configs.models
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
@@ -14,19 +13,23 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo
     JsonSubTypes.Type(value = MetricsToAddTarget.Name::class, name = "name"),
     JsonSubTypes.Type(value = MetricsToAddTarget.Attribute::class, name = "attribute"),
 )
-@JsonIgnoreProperties(ignoreUnknown = true)
 sealed class MetricsToAddTarget {
 
-    @JsonIgnoreProperties(ignoreUnknown = true)
     data class Name(
-        var type: String? = null,
+        @get:JsonProperty("type")
+        @param:JsonProperty("type")
+        val type: String? = null,
     ) : MetricsToAddTarget()
 
-    @JsonIgnoreProperties(ignoreUnknown = true)
     data class Attribute(
-        var type: String? = null,
-        var condition: EventFilter? = null,
+        @get:JsonProperty("type")
+        @param:JsonProperty("type")
+        val type: String? = null,
+        @get:JsonProperty("condition")
+        @param:JsonProperty("condition")
+        val condition: EventFilter? = null,
         @get:JsonProperty("shouldAddPropNameAsSuffix")
-        var shouldAddPropNameAsSuffix: Boolean = false,
+        @param:JsonProperty("shouldAddPropNameAsSuffix")
+        val shouldAddPropNameAsSuffix: Boolean = false,
     ) : MetricsToAddTarget()
 }
