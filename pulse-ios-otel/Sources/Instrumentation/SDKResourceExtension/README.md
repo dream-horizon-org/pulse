bd9d93f6079818d135cdf1daef323193bb0abe7e
Shubham <85783070+shubhamguptadream11@users.noreply.github.com>
2026-04-10 15:47:01 +0530

    feat: pulse ios support  (#472)
    * feat: ios support
    
    * fix: swift lint
    
    * fix: swift lint
    
    * feat: readme fixes
    
    * feat: author name chaned
    
    * feat: readme updated
    
    * feat: readme updated
    
    * feat: spm project file fix for missing ref
    
    * fix: dummy url
    
    * fix: ios deployment target 15 to 13

---
+++ pulse-ios-otel/Sources/Instrumentation/SDKResourceExtension/README.md
@@ -0,0 +1,7 @@
+# SDK Resource extension
+
+This extension captures key application data points and generates a `Resource` object for `TracerProvider`.
+
+## Usage
+
+Use `DefaultResource.get()` to generate an all-in-one resource object.
---
+++ pulse-ios-otel/Tests/InstrumentationTests/SDKResourceExtensionTests/Mocks/MockApplicationDataSource.swift
@@ -0,0 +1,21 @@
+/*
+ * Copyright The Pulse Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import Foundation
+@testable import PulseKit
+
+class mockApplicationData: IApplicationDataSource {
+  var name: String?
+  var identifier: String?
+  var version: String?
+  var build: String?
+
+  init(name: String? = nil, identifier: String? = nil, version: String? = nil, build: String? = nil) {
+    self.name = name
+    self.identifier = identifier
+    self.version = version
+    self.build = build
+  }
+}
