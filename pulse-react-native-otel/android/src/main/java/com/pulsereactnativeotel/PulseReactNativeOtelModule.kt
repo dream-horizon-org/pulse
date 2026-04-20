35451bc921663895450bd63f710fa6f39e0393cd
anirudhbharti <anirudh.bharti@dream11.com>
2026-04-20 16:17:48 +0530

    fix: add logs

--- pulse-react-native-otel/android/src/main/java/com/pulsereactnativeotel/PulseReactNativeOtelModule.kt
+++ pulse-react-native-otel/android/src/main/java/com/pulsereactnativeotel/PulseReactNativeOtelModule.kt
@@ -9,7 +9,7 @@ import com.pulse.android.api.otel.PulseDataCollectionConsent
 import com.pulse.android.sdk.internal.PulseSDKInternal
 import android.content.Context
 import android.os.Looper
-import android.util.Log
+import com.pulse.utils.PulseLogger
 import android.os.Handler
 import com.pulse.sampling.models.PulseSdkConfig
 import com.pulse.sampling.models.PulseSdkName
@@ -106,7 +106,9 @@ internal class PulseReactNativeOtelModule(reactContext: ReactApplicationContext)
 
   override fun triggerAnr() {
     Handler(Looper.getMainLooper()).postAtFrontOfQueue {
-      Log.d("[Pulse]", "Now running PostAtFrontQueue: ${Thread.currentThread().name}")
+      PulseLogger.logDebug("PulseRN") {
+        "triggerAnr: PostAtFrontOfQueue on ${Thread.currentThread().name}"
+      }
       Thread.sleep(10_000)
     }
   }
