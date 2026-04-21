60bad5698c18bcdb2fa8fc2c385732ce1940422b
anirudhbharti <anirudh.bharti@dream11.com>
2026-04-21 12:42:32 +0530

    fix: checks

--- pulse-android-otel/pulse-utils/src/main/java/com/pulse/utils/RedactionUtils.kt
+++ pulse-android-otel/pulse-utils/src/main/java/com/pulse/utils/RedactionUtils.kt
@@ -9,19 +9,27 @@ import javax.net.ssl.SSLException
 public object RedactionUtils {
     public fun classifyError(throwable: Throwable): String =
         when (throwable) {
-            is SocketTimeoutException -> "timeout"
-            is UnknownHostException -> "dns_resolution_failed"
-            is ConnectException -> "connection_refused"
-            is SSLException -> "ssl_error"
-            is IOException -> "io_error"
+            is SocketTimeoutException -> {
+                "timeout"
+            }
+            is UnknownHostException -> {
+                "dns_resolution_failed"
+            }
+            is ConnectException -> {
+                "connection_refused"
+            }
+            is SSLException -> {
+                "ssl_error"
+            }
+            is IOException -> {
+                "io_error"
+            }
             is retrofit2.HttpException -> {
                 val code = throwable.code()
-                when {
-                    code in 400..499 -> "http_$code"
-                    code in 500..599 -> "http_$code"
-                    else -> "http_$code"
-                }
+                "http_$code"
             }
-            else -> "unknown"
+            else -> {
+                "unknown"
+            }
         }
 }
