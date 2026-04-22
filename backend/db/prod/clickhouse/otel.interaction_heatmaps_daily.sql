65c5c90b6724fd9add91782d0744856e9df28174
Mohd. Yasir Ansari <yasir.ansari@dream11.com>
2026-04-23 01:15:37 +0530

    chore: added heatmap in prod clickhouse

---
+++ backend/db/prod/clickhouse/otel.interaction_heatmaps_daily.sql
@@ -0,0 +1,49 @@
+CREATE TABLE IF NOT EXISTS otel.interaction_heatmaps_daily_local
+ON CLUSTER 'pulse-clickhouse'
+(
+    `Date` Date,
+    `ProjectId` LowCardinality(String),
+    `ScreenName` LowCardinality(String),
+    `AppVersion` LowCardinality(String),
+    `Platform` LowCardinality(String),
+    `GeographicalRegion` LowCardinality(String),
+    `Breakpoint` LowCardinality(String),
+    `XBin` Float32,
+    `YBin` Float32,
+    `WeightNormal` UInt64,
+    `WeightRage` UInt64,
+    `WeightDead` UInt64
+    )
+    ENGINE = SummingMergeTree()
+    ORDER BY (Date, ProjectId, ScreenName, AppVersion, Platform, GeographicalRegion, Breakpoint, XBin, YBin);
+
+CREATE TABLE IF NOT EXISTS otel.interaction_heatmaps_daily
+ON CLUSTER 'pulse-clickhouse'
+AS otel.interaction_heatmaps_daily_local
+ENGINE = Distributed('pulse-clickhouse', otel, interaction_heatmaps_daily_local, cityHash64((ProjectId)));
+
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS otel.interaction_heatmaps_daily_mv
+TO otel.interaction_heatmaps_daily AS
+SELECT
+    toDate(Timestamp) AS Date,
+    ProjectId,
+    ScreenName,
+    AppVersion,
+    Platform,
+    GeoState AS GeographicalRegion,
+    multiIf(
+        Platform = 'Web' AND ViewportWidth > 1024, 'Web_Extra_Large',
+        Platform = 'Web', 'Mobile_Medium',
+        ViewportWidth > 600, 'Tablet_Large',
+        ViewportWidth <= 600 AND (ViewportHeight / ViewportWidth) <= 1.5, 'Mobile_Small',
+        'Mobile_Medium'
+    ) AS Breakpoint,
+    round(NormXPer, 2) AS XBin,
+    round(NormYPer, 2) AS YBin,
+    count() AS WeightNormal,
+    countIf(Rage) AS WeightRage,
+    countIf(ClickType = 'dead') AS WeightDead
+FROM otel.otel_logs
+WHERE PulseType = 'app.click'
+GROUP BY Date, ProjectId, ScreenName, AppVersion, Platform, GeoState, Breakpoint, XBin, YBin;
