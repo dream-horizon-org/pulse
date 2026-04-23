5d973bbb090ee9f011c8b5d197711fc76a5c14f7
Thotaharshavardhani <harshavardhani.thota@dream11.com>
2026-04-23 16:02:48 +0530

    Internal Viewer CRUD apis

---
+++ backend/server/src/main/java/org/dreamhorizon/pulseserver/resources/v1/admin/models/AssignInternalViewerRequest.java
@@ -0,0 +1,11 @@
+package org.dreamhorizon.pulseserver.resources.v1.admin.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class AssignInternalViewerRequest {
+
+  @JsonProperty("userId")
+  private String userId;
+}
