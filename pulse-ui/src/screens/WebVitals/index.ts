fb21029b759fdad8ab653c16303e21bac6844d6a
JatinDream11 <jatin.khemchandani@dream11.com>
2026-05-13 13:24:52 +0530

    chore minor fix

--- pulse-ui/src/screens/WebVitals/index.ts
+++ pulse-ui/src/screens/WebVitals/index.ts
@@ -1,5 +1,5 @@
 export * from "./WebVitals.interface";
 export * from "./WebVitalsWire.types";
 export * from "./WebVitals.constants";
-export * from "./components";
 export * from "./hooks";
+export * from "./components";
